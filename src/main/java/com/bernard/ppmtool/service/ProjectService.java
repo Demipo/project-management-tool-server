536eac67227d3b08f3408ee7049b052143c9e1f9
Demipo <41957499+Demipo@users.noreply.github.com>
2020-03-18 12:40:04 +0100

    Create custom exception handling for project identifier uniqueness

--- src/main/java/com/bernard/ppmtool/service/ProjectService.java
+++ src/main/java/com/bernard/ppmtool/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.bernard.ppmtool.service;
 
 import com.bernard.ppmtool.domain.Project;
+import com.bernard.ppmtool.exception.ProjectIdExceptionHandler;
 import com.bernard.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception e){
+            throw new ProjectIdExceptionHandler("Project identifier " + project.getProjectIdentifier() + " already exists");
+        }
     }
 }
