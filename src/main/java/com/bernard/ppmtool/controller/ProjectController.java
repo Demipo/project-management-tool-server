755d9a320bf11498b7156399f2bc390bef515623
Demipo <41957499+Demipo@users.noreply.github.com>
2020-03-18 11:12:09 +0100

    remove error handling logic from controller

--- src/main/java/com/bernard/ppmtool/controller/ProjectController.java
+++ src/main/java/com/bernard/ppmtool/controller/ProjectController.java
@@ -1,17 +1,22 @@
 package com.bernard.ppmtool.controller;
 
 import com.bernard.ppmtool.domain.Project;
+import com.bernard.ppmtool.service.MapValidationErrorService;
 import com.bernard.ppmtool.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("ap1/project")
@@ -20,12 +25,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
 
     @RequestMapping(path = "", method = RequestMethod.POST)
     public ResponseEntity<?> createNewProject(@Valid  @RequestBody Project project,  BindingResult result){
-        if(result.hasErrors()){
-            return  new ResponseEntity<>("Invalid request parameter(s)", HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity errorMap = mapValidationErrorService.mapValidationError(result);
+        if(errorMap != null) return errorMap;
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
